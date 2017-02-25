4c7467451a332c0ade8d80d0ba0e7a31e6d42559
Mikołaj Morzy <mikolaj.morzy@gmail.com>
2017-02-25 23:28:52 +0100

    Corrected plot labels, changed theme to b&w

--- analysis.R
+++ analysis.R
@@ -3,7 +3,7 @@ library(reshape2)
 library(ggplot2)
 
 # read text file with results
-results <- read.csv(file = 'results.100.50.ws.er.csv')
+results <- read.csv(file = 'results.100.50.ws.b.csv')
 # transform the data into the long format
 results_long <- melt(results, id = 'p')
 
@@ -11,7 +11,7 @@ results_long <- melt(results, id = 'p')
 results_long$type <- ''
 
 results_long[results_long$variable == 'eA' | results_long$variable == 'kA',]$type <- 'adjacency matrix'
-results_long[results_long$variable == 'eL' | results_long$variable == 'kL',]$type <- 'laplacian'
+results_long[results_long$variable == 'eL' | results_long$variable == 'kL',]$type <- 'Laplacian matrix'
 results_long[results_long$variable == 'ed' | results_long$variable == 'kd',]$type <- 'degree list'
 results_long[results_long$variable == 'eD' | results_long$variable == 'kD',]$type <- 'degree distribution'
 
@@ -22,19 +22,19 @@ results_long[results_long$variable == 'eA',]$class <- 'entropy'
 results_long[results_long$variable == 'eL',]$class <- 'entropy'
 results_long[results_long$variable == 'ed',]$class <- 'entropy'
 results_long[results_long$variable == 'eD',]$class <- 'entropy'
-results_long[results_long$variable == 'kA',]$class <- 'complexity'
-results_long[results_long$variable == 'kL',]$class <- 'complexity'
-results_long[results_long$variable == 'kd',]$class <- 'complexity'
-results_long[results_long$variable == 'kD',]$class <- 'complexity'
+results_long[results_long$variable == 'kA',]$class <- 'K-complexity'
+results_long[results_long$variable == 'kL',]$class <- 'K-complexity'
+results_long[results_long$variable == 'kd',]$class <- 'K-complexity'
+results_long[results_long$variable == 'kD',]$class <- 'K-complexity'
 
 # factorize the variable to have a more readable labels in the legend
 results_long$variable <- factor(results_long$variable, levels = c('eA','eL','ed','eD','kA','kL','kd','kD'), 
-                                labels = c('entropy of adjacency matrix','entropy of laplacian','entropy of degree list','entropy of degree distribution',
-                                           'complexity of adjacency matrix','complexity of laplacian','complexity of degree list','complexity of degree distribution'))
+                                labels = c('entropy of adjacency matrix','entropy of Laplacian matrix','entropy of degree list','entropy of degree distribution',
+                                           'K-complexity of adjacency matrix','K-complexity of Laplacian matrix','K-complexity of degree list','K-complexity of degree distribution'))
 
 # generate the 2D plot factorized by the type of network measure
-plt <- ggplot(results_long, aes(x = p, y = value, color = variable)) + geom_line() + facet_wrap(~ type, ncol = 2)
+plt <- ggplot(results_long, aes(x = p, y = value, color = variable, linetype = variable)) + geom_line() + facet_wrap(~ type, ncol = 2) + theme_bw()
 plt
 
 # save the plot
-ggsave(filename = 'results.100.50.ws.er.png')
+ggsave(filename = 'results.100.50.ws.b.png')
